aeb492576144281b5f5ff51882e8533d22d97e70
Corky Brown <galaxyblur@gmail.com>
2024-06-11 16:02:55 -0400

    Fix vim options

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -2,7 +2,13 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "x", '"_x')
+
 local opt = vim.opt
+
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
 
 opt.relativenumber = true
 opt.number = true
@@ -12,12 +18,17 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.list = true
 
 opt.wrap = false
+opt.scrolloff = 10
+opt.inccommand = "split"
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.path:append({ "**" })
+opt.wildignore:append({ "*/node_modules/*" })
 
 opt.cursorline = true
 
@@ -26,7 +37,7 @@ opt.background = "dark" -- colorschemes that can be light or dark will be made d
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = { "indent", "eol", "start" } -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
