b9e9e3c2e1224301496031f44545a3e78b850286
Corky Brown <galaxyblur@gmail.com>
2024-06-11 22:34:59 -0400

    Switch to AstroNvim

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-	"nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
   },
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			--[[
-      ensure_installed = {
-        -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-        "c", "lua", "vim", "vimdoc", "query",
-        "bash",
-        "javascript", "typescript", "json", "jsdoc",
-        "html", "css", "scss", "vue",
-        "xml", "sql", "php", "java", "tsx",
-        -- "tmux",
-        "python", "swift", "kotlin", "svelte", "ruby", "rust", "go",
-        "dockerfile", "yaml", "toml", "regex", "csv",
-        "gdscript",
-        -- "gdresource", "gdshader",
-        "glsl", "wgsl",
-        "gitignore", "gitcommit", "gitattributes", "git_config", "git_rebase",
-      },
-      ]]
-			highlight = { enable = true },
-			indent = { enable = true },
-      autotag = { enable = true },
-		})
-	end,
 }
