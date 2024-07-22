c60de21c265f96e873b32101f16aaa5bd11d8565
Corky Brown <galaxyblur@gmail.com>
2024-07-21 23:46:22 -0400

    add text wrap to md files

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -1,18 +1,26 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+--
+-- -- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- Create an autocommand group to avoid duplication
+vim.api.nvim_create_augroup("WrapTextFiles", { clear = true })
+
+-- Enable text wrapping for markdown and text files
+vim.api.nvim_create_autocmd("FileType", {
+  group = "WrapTextFiles",
+  pattern = { "markdown", "text" },
+  command = "setlocal wrap",
+})
