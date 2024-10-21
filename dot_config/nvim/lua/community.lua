bfd0aeceabd22cc5e836d600fe5904919ccbbdcd
Corky Brown <galaxyblur@gmail.com>
2024-10-20 23:14:12 -0400

    Refresh astro nvim config

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -7,5 +7,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.vue" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
 }
